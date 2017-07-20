819e8fe98ea54d2aede4a24b1d828a2509160223
Luiz Souza <luiz@netgate.com>
2017-07-20 14:40:04 -0500

    Add support for the classic pfSense 'mixed' tables.
    The mixed tables are used to match against the IP[4|6] and the MAC address of the peer.
    
    (cherry picked from commit edfbe4e11e3f835451d36dfb533e0785f4437497)

--- sys/netpfil/ipfw/ip_fw_private.h
+++ sys/netpfil/ipfw/ip_fw_private.h
@@ -784,10 +784,10 @@ int ipfw_reset_eaction_instance(struct ip_fw_chain *ch, uint16_t eaction_id,
 struct table_info;
 
 typedef int (table_lookup_t)(struct table_info *ti, void *key, uint32_t keylen,
-    uint32_t *val, void **te);
+    uint32_t *val, uint8_t *ea, void **te);
 
 int ipfw_lookup_table(struct ip_fw_chain *ch, uint16_t tbl, uint16_t plen,
-    void *paddr, uint32_t *val);
+    void *paddr, uint32_t *val, uint8_t *ea, void **te);
 void ipfw_cnt_update_tentry(struct ip_fw_chain *ch, uint16_t tbl, uint16_t plen,
     void *e, int pktlen);
 struct named_object *ipfw_objhash_lookup_table_kidx(struct ip_fw_chain *ch,
