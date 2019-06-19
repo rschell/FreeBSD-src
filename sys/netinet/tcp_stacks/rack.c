8942920c3a4a5c45537fe8776f48b99e6ce1e2c9
gordon <gordon@FreeBSD.org>
2019-06-19 16:43:05 +0000

    Add ability to limit split map entries to prevent resource exhaustion in non-default RACK TCP stack.
    Approved by:	so
    Security:	 FreeBSD-SA-19:08.rack
    Security:	 CVE-2019-5599

--- sys/netinet/tcp_stacks/rack.c
+++ sys/netinet/tcp_stacks/rack.c
@@ -1,5 +1,6 @@
 /*-
- * Copyright (c) 2016-2018 Netflix, Inc.
+ * Copyright (c) 2016-2019
+ *	Netflix Inc.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
